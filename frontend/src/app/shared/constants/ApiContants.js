0fe63ca8c140a9517d310e91f7be7c4b095f30bd
liedsonlb <liedson.b9@gmail.com>
2025-04-23 18:08:23 -0300

    fix: update apiConstants

--- frontend/src/app/shared/constants/ApiContants.js
+++ frontend/src/app/shared/constants/ApiContants.js
@@ -1,3 +1,3 @@
-export const DOMAIN = "/api/"
-// export const DOMAIN = "http://backend:8000/"
+// export const DOMAIN = "/api/"
+export const DOMAIN = "http://backend:8000/"
 export const AppKey = "W3L3A"
