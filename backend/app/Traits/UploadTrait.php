690912364f44a14e1803dcffcee660bce29dd96d
liedsonlb <liedson.b9@gmail.com>
2025-05-14 15:51:25 -0300

    fix: update  mobifile service storage for local storage

--- backend/app/Traits/UploadTrait.php
+++ backend/app/Traits/UploadTrait.php
@@ -9,7 +9,8 @@ use Intervention\Image\ImageManagerStatic as Image;
 use App\Exceptions\ValidationException;
 use App\Storage\LocalImageStorage;
 
-trait UploadTrait{
+trait UploadTrait
+{
 
     /**
      *
@@ -26,12 +27,20 @@ trait UploadTrait{
      * @return string|null retorna o caminho do arquivo salvo ou null
      * @throws \Exception caso alguma validacao barre
      */
-    public function upload(Request $request, string $field, string $folder, int $file_size_max = 1000,
-                           int $img_size_max = 1000, array|string $types = [], bool $thumb = false,
-                           int $thumbSize = 120, bool $public = true) : ?string {
+    public function upload(
+        Request $request,
+        string $field,
+        string $folder,
+        int $file_size_max = 1000,
+        int $img_size_max = 1000,
+        array|string $types = [],
+        bool $thumb = false,
+        int $thumbSize = 120,
+        bool $public = true
+    ): ?string {
 
         //caso seja usado link no lugar de upload
-        if($request->filled($field.'_url')) return $request->get($field.'_url');
+        if ($request->filled($field . '_url')) return $request->get($field . '_url');
 
         $file = $request->file($field);
         return $this->uploadFile($file, $field, $folder, $file_size_max, $img_size_max, $types, $thumb, $thumbSize, $public);
@@ -53,58 +62,69 @@ trait UploadTrait{
      * @return string|null retorna o caminho do arquivo salvo ou null
      * @throws \Exception caso alguma validacao barre
      */
-    public function uploadFile(?UploadedFile $file, string $field, string $folder, int $file_size_max = 1000,
-                               int $img_size_max = 1000, array|string $types = [], bool $thumb = false,
-                               int $thumbSize = 120, bool $public = true) : ?string {
+    public function uploadFile(
+        ?UploadedFile $file,
+        string $field,
+        string $folder,
+        int $file_size_max = 1000,
+        int $img_size_max = 1000,
+        array|string $types = [],
+        bool $thumb = false,
+        int $thumbSize = 120,
+        bool $public = true
+    ): ?string {
 
-        if($file == null) return null;
+        if ($file == null) return null;
 
-        if(is_array($types)) $types = implode(",", $types);
+        if (is_array($types)) $types = implode(",", $types);
 
         $validation = [];
-        if(!empty($types)) $validation[] = 'mimes:'.$types;
-        if($file_size_max > 0) $validation[] = 'max:'.$file_size_max;
+        if (!empty($types)) $validation[] = 'mimes:' . $types;
+        if ($file_size_max > 0) $validation[] = 'max:' . $file_size_max;
 
         $validation = implode("|", $validation);
-        if(!empty($validation)){
-                $validator = Validator::make([$field => $file], [
-                    $field => $validation
-                ]);
-                if($validator->fails())
-                    throw new ValidationException($validator, $this->model ?? null);
+        if (!empty($validation)) {
+            $validator = Validator::make([$field => $file], [
+                $field => $validation
+            ]);
+            if ($validator->fails())
+                throw new ValidationException($validator, $this->model ?? null);
         }
 
         $erro = $this->getErrorMsg($file->getError());
-        if(!empty($erro))  throw new \Exception($erro);
+        if (!empty($erro))  throw new \Exception($erro);
 
-        if($img_size_max > 0){
+        if ($img_size_max > 0) {
             Image::make($file->getRealPath())->resize($img_size_max, $img_size_max, function ($constraint) {
                 $constraint->aspectRatio();
                 $constraint->upsize();
             })->save();
         }
 
-        $file_name = md5(uniqid(time())).'.'.$file->getClientOriginalExtension();
+        $file_name = md5(uniqid(time())) . '.' . $file->getClientOriginalExtension();
         $repo = LocalImageStorage::makeDefault();
         $arq = $repo->upload($file->getRealPath(), $file_name, $folder, $file_size_max * 1024); //em bytes
 
-        if(strlen($arq->url) > 0){
-            if($thumb) $repo->thumb($arq->url, 120);
-            return $arq->url;
+        if (strlen($arq->path) > 0) {
+            if ($thumb) {
+                $repo->thumb($arq->path, 120);
+            }
+            return $arq->path;
         }
 
         return null;
-
     }
 
-    public function deleteFile(string $path): bool{
+    public function deleteFile(string $path): bool
+    {
         LocalImageStorage::makeDefault()->remove($path);
         return true;
     }
 
-    private function getErrorMsg($error): string{
+    private function getErrorMsg($error): string
+    {
         $phpFileUploadErrors = array(
-//            0 => 'There is no error, the file uploaded with success',
+            //            0 => 'There is no error, the file uploaded with success',
             1 => 'Arquivo excede o máximo permitido na configuração do servidor!',
             2 => 'Arquivo excede o máximo permitido na configuração do formulário!',
             3 => 'Arquivo com upload parcial!',
@@ -116,5 +136,4 @@ trait UploadTrait{
 
         return $phpFileUploadErrors[$error] ?? '';
     }
-
 }
